c7a044d5b3525b0cdc6ae4284536efbd134116da
ZebraPillow <152799948+ZebraPillow@users.noreply.github.com>
2026-02-02 09:29:28 +0800

    Delete Tetris.tools.h

--- Tetris.tools.h
+++
@@ -1,8 +0,0 @@
-#define STTY_US  "stty raw -echo -F "
-#define STTY_DEF  "stty -raw echo -F "
-
-#define START_INPUT system( STTY_US  TTY_PATH ); 
-#define END_INPUT system( STTY_DEF TTY_PATH );
-
-void drawPoint(int x, int y, int color);
-int myget();
